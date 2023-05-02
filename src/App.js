1a7f8b8115cbd05b0e98214d8934c518fa8fdbbe
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-02 01:08:12 +0100

    fetch and search data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './Pages/HomePage';
+import Header from './Componets/Header';
+import Footer from './Componets/Footer';
+import SearchPage from './Pages/SearchPage';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/search' element={<SearchPage/>}/>
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
