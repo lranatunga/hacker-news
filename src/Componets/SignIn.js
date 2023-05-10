0569fa07c62d4a936ea52a79f1db1a619d37f97b
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-10 20:48:44 +0100

    conflict corrected

--- src/Componets/SignIn.js
+++ src/Componets/SignIn.js
@@ -1,10 +1,20 @@
-
+import { NavLink } from "react-router-dom";
 
 export default function  LogIn () {
 
 
     return (
-        <></>
+        <div class="LogInButton">
+        <nav><NavLink 
+                to='/login'
+                style={({isActive}) => {
+                    return {
+                        color: isActive ? 'orange' : 'white',
+                        textDecoration:'none',
+                        marginTop:'5px'
+                    }
+                }}><button class="btn-log-in">Log In</button></NavLink></nav>
+    </div>
         // <div className="LogInButton">
         //     <button className="btn-log-in">Log In</button>
         // </div>
